a11501858e50fb650de043d2b34b222484f97fb6
Josh Lowe <joshtylerlowe@gmail.com>
2017-12-14 23:28:25 -0700

    added preference to change orb names; added beep before voice cue;

--- DhuumTimer.js
+++ DhuumTimer.js
@@ -1,8 +1,9 @@
-var CCOUNT = 10 * 60;//10 minutes
+var CCOUNT = (10 * 60) + 10;//10 minutes
+var singlebeep = new Audio('beeplow.mp3');
 var upboing = new Audio('upboing.mp3');
 var chordbeep = new Audio('chordbeep.mp3');
 var goingdownzip = new Audio('goingdownzip.mp3');
-var t, count, lastText;
+var t, count, lastText, orb1Val, orb2Val, orb3Val;
 var orb1 = orb2 = orb3 = orbwarning = orbup = sswarning = ssup = voice = chime = nosound = false;
 
 $(document).ready(function () {
@@ -64,54 +65,54 @@ $(document).ready(function () {
 
 var displayData = [
     { time: 600, type: "", text: "" },
-    { time: 580, type: "orbwarning,orb1", text: "Orb 1 to Arrow" },
-    { time: 570, type: "orbup,orb1", text: "Orb 1 going up" },
-    { time: 550, type: "orbwarning,orb2", text: "Orb 2 to Circle" },
-    { time: 540, type: "orbup,orb2", text: "Orb 2 going up, bombs starting" },
-    { time: 520, type: "orbwarning,orb3", text: "Orb 3 to Heart" },
-    { time: 510, type: "orbup,orb3", text: "Orb 3 going up" },
-    { time: 490, type: "orbwarning,orb1", text: "Orb 1 to Square" },
-    { time: 480, type: "orbup,orb1", text: "Orb 1 going up, Dhuum is activated" },
-    { time: 460, type: "orbwarning,orb2", text: "Orb 2 to Star" },
-    { time: 450, type: "orbup,orb2", text: "Orb 2 going up" },
-    { time: 430, type: "orbwarning,orb3", text: "Orb 3 to Spiral" },
-    { time: 420, type: "orbup,orb3", text: "Orb 3 going up" },
-    { time: 400, type: "orbwarning,orb1", text: "Orb 1 to Triangle" },
+    { time: 580, type: "orbwarning,orb1", text: "{1} to Arrow" },
+    { time: 570, type: "orbup,orb1", text: "{1} going up" },
+    { time: 550, type: "orbwarning,orb2", text: "{2} to Circle" },
+    { time: 540, type: "orbup,orb2", text: "{2} going up, bombs starting" },
+    { time: 520, type: "orbwarning,orb3", text: "{3} to Heart" },
+    { time: 510, type: "orbup,orb3", text: "{3} going up" },
+    { time: 490, type: "orbwarning,orb1", text: "{1} to Square" },
+    { time: 480, type: "orbup,orb1", text: "{1} going up, Boss is activated" },
+    { time: 460, type: "orbwarning,orb2", text: "{2} to Star" },
+    { time: 450, type: "orbup,orb2", text: "{2} going up" },
+    { time: 430, type: "orbwarning,orb3", text: "{3} to Spiral" },
+    { time: 420, type: "orbup,orb3", text: "{3} going up" },
+    { time: 400, type: "orbwarning,orb1", text: "{1} to Triangle" },
     { time: 395, type: "sswarning", text: "prepare for soul slam" },
-    { time: 390, type: "orbup,orb1", text: "Orb 1 going up" },
-    { time: 385, type: "ssup", text: "SOUL SLAM NOW" },
-    { time: 370, type: "orbwarning,orb2", text: "Orb 2 to Arrow" },
-    { time: 360, type: "orbup,orb2", text: "Orb 2 going up" },
-    { time: 340, type: "orbwarning,orb3", text: "Orb 3 to Circle" },
-    { time: 330, type: "orbup,orb3", text: "Orb 3 going up" },
-    { time: 315, type: "sswarning,orbwarning,orb1", text: "prepare for soul slam, Orb 1 to Heart AFTER soul slam" },
-    { time: 305, type: "ssup", text: "SOUL SLAM NOW" },
-    { time: 300, type: "orbup,orb1", text: "Orb 1 going up" },
-    { time: 280, type: "orbwarning,orb2", text: "Orb 2 to Square" },
-    { time: 270, type: "orbup,orb2", text: "Orb 2 going up" },
-    { time: 250, type: "orbwarning,orb3", text: "Orb 3 to Star" },
-    { time: 240, type: "orbup,orb3", text: "Orb 3 going up" },
+    { time: 390, type: "orbup,orb1", text: "{1} going up" },
+    { time: 385, type: "ssup", text: "sole slam! now" },
+    { time: 370, type: "orbwarning,orb2", text: "{2} to Arrow" },
+    { time: 360, type: "orbup,orb2", text: "{2} going up" },
+    { time: 340, type: "orbwarning,orb3", text: "{3} to Circle" },
+    { time: 330, type: "orbup,orb3", text: "{3} going up" },
+    { time: 315, type: "sswarning,orbwarning,orb1", text: "prepare for soul slam, {1} to Heart AFTER soul slam" },
+    { time: 305, type: "ssup", text: "sole slam! now" },
+    { time: 300, type: "orbup,orb1", text: "{1} going up" },
+    { time: 280, type: "orbwarning,orb2", text: "{2} to Square" },
+    { time: 270, type: "orbup,orb2", text: "{2} going up" },
+    { time: 250, type: "orbwarning,orb3", text: "{3} to Star" },
+    { time: 240, type: "orbup,orb3", text: "{3} going up" },
     { time: 235, type: "sswarning", text: "prepare for soul slam" },
-    { time: 225, type: "ssup", text: "SOUL SLAM NOW" },
-    { time: 220, type: "orbwarning,orb1", text: "Orb 1 to Spiral" },
-    { time: 210, type: "orbup,orb1", text: "Orb 1 going up" },
-    { time: 190, type: "orbwarning,orb2", text: "Orb 2 to Triangle" },
-    { time: 180, type: "orbup,orb2", text: "Orb 2 going up" },
-    { time: 160, type: "orbwarning,orb3", text: "Orb 3 to Arrow" },
+    { time: 225, type: "ssup", text: "sole slam! now" },
+    { time: 220, type: "orbwarning,orb1", text: "{1} to Spiral" },
+    { time: 210, type: "orbup,orb1", text: "{1} going up" },
+    { time: 190, type: "orbwarning,orb2", text: "{2} to Triangle" },
+    { time: 180, type: "orbup,orb2", text: "{2} going up" },
+    { time: 160, type: "orbwarning,orb3", text: "{3} to Arrow" },
     { time: 155, type: "sswarning", text: "prepare for soul slam" },
-    { time: 150, type: "orbup,orb3", text: "Orb 3 going up" },
-    { time: 145, type: "ssup", text: "SOUL SLAM NOW" },
-    { time: 130, type: "orbwarning,orb1", text: "Orb 1 to Circle" },
-    { time: 120, type: "orbup,orb1", text: "Orb 1 going up" },
-    { time: 100, type: "orbwarning,orb2", text: "Orb 2 to Heart" },
-    { time: 90, type: "orbup,orb2", text: "Orb 2 going up" },
-    { time: 75, type: "sswarning,orbwarning,orb3", text: "prepare for soul slam, Orb 3 to Square AFTER soul slam" },
-    { time: 65, type: "ssup", text: "SOUL SLAM NOW" },
-    { time: 60, type: "orbup,orb3", text: "Orb 3 going up, 1 minute until enrage" },
-    { time: 40, type: "orbwarning,orb1", text: "Orb 1 to Star" },
-    { time: 30, type: "orbup,orb1", text: "Orb 1 going up, 30 seconds until enrage" },
-    { time: 10, type: "orbwarning,orb2", text: "Orb 2 to Spiral" },
-    { time: 0, type: "orbup,orb2", text: "Orb 2 going up, ENRAGE" }
+    { time: 150, type: "orbup,orb3", text: "{3} going up" },
+    { time: 145, type: "ssup", text: "sole slam! now" },
+    { time: 130, type: "orbwarning,orb1", text: "{1} to Circle" },
+    { time: 120, type: "orbup,orb1", text: "{1} going up" },
+    { time: 100, type: "orbwarning,orb2", text: "{2} to Heart" },
+    { time: 90, type: "orbup,orb2", text: "{2} going up" },
+    { time: 75, type: "sswarning,orbwarning,orb3", text: "prepare for soul slam, {3} to Square AFTER soul slam" },
+    { time: 65, type: "ssup", text: "sole slam! now" },
+    { time: 60, type: "orbup,orb3", text: "{3} going up, 1 minute until enrage" },
+    { time: 40, type: "orbwarning,orb1", text: "{1} to Star" },
+    { time: 30, type: "orbup,orb1", text: "{1} going up, 30 seconds until enrage" },
+    { time: 10, type: "orbwarning,orb2", text: "{2} to Spiral" },
+    { time: 0, type: "orbup,orb2", text: "{2} going up, ENRAGE" }
 ];
 
 function cddisplay() {
@@ -153,6 +154,9 @@ function cdstart() {
     $('#stopButton').show();
     $('#resetButton').show();
 
+    orb1Val = $('#orb1text').val().length > 0 ? $('#orb1text').val() : "";
+    orb2Val = $('#orb2text').val().length > 0 ? $('#orb2text').val() : "";
+    orb3Val = $('#orb3text').val().length > 0 ? $('#orb3text').val() : "";
     orb1 = $('#orb1').is(':checked');
     orb2 = $('#orb2').is(':checked');
     orb3 = $('#orb3').is(':checked');
@@ -171,38 +175,65 @@ function cdstart() {
     countdown();
 }
 
-var getDisplayText = function(seconds) {
-    for (var i = 1; i < displayData.length; i++) {
-        if (displayData[i].time < count) {
-            var orb1check = orb1 && displayData[i-1].type.indexOf('orb1') >= 0;
-            var orb2check = orb2 && displayData[i-1].type.indexOf('orb2') >= 0;
-            var orb3check = orb3 && displayData[i-1].type.indexOf('orb3') >= 0;
-            //var orbWarningCheck = (orb1check || orb2check || orb3check) && orbwarning && displayData[i - 1].type.indexOf('orbwarning') >= 0;
-            //var orbUpCheck = (orb1check || orb2check || orb3check) && orbup && displayData[i - 1].type.indexOf('orbup') >= 0;
-            //var ssWarningCheck = sswarning && displayData[i-1].type.indexOf('sswarning') >= 0;
-            //var ssUpCheck = ssup && displayData[i-1].type.indexOf('ssup') >= 0;
-            var ssCheck = ssup && (displayData[i - 1].type.indexOf('sswarning') >= 0 || displayData[i-1].type.indexOf('ssup') >= 0);
+var getDisplayText = function (seconds) {
+    if (seconds >= 600) {
 
-            if (orb1check || orb2check || orb3check /*|| orbWarningCheck || orbUpCheck || ssWarningCheck || ssUpCheck*/ || ssCheck) {
-                if (lastText != displayData[i - 1].text && seconds <= displayData[1].time) {
-                    lastText = displayData[i - 1].text;
+        if (seconds == 605) {
+            responsiveVoice.speak("fight starting", "UK English Male");
+            return "fight starting";
+        } else if (seconds == 603) {
+            responsiveVoice.speak("3", "UK English Male");
+            return "3";
+        } else if (seconds == 602) {
+            responsiveVoice.speak("2", "UK English Male");
+            return "2";
+        } else if (seconds == 601) {
+            responsiveVoice.speak("1", "UK English Male");
+            return "1";
+        } else if (seconds == 600) {
+            responsiveVoice.speak("Go!", "UK English Male");
+            return "Go!";
+        }
+    } else {
 
-                    if (voice) {
-                        responsiveVoice.speak(lastText);
-                    } else if (chime) {
-                        if (orb1check || orb2check || orb3check) {
-                            upboing.play();
+        for (var i = 0; i < displayData.length; i++) {
+            if (displayData[i + 1] && displayData[i + 1].time < count) {
+                var orb1check = orb1 && displayData[i].type.indexOf('orb1') >= 0;
+                var orb2check = orb2 && displayData[i].type.indexOf('orb2') >= 0;
+                var orb3check = orb3 && displayData[i].type.indexOf('orb3') >= 0;
+                //var orbWarningCheck = (orb1check || orb2check || orb3check) && orbwarning && displayData[i - 1].type.indexOf('orbwarning') >= 0;
+                //var orbUpCheck = (orb1check || orb2check || orb3check) && orbup && displayData[i - 1].type.indexOf('orbup') >= 0;
+                //var ssWarningCheck = sswarning && displayData[i-1].type.indexOf('sswarning') >= 0;
+                //var ssUpCheck = ssup && displayData[i-1].type.indexOf('ssup') >= 0;
+                var ssCheck = ssup && (displayData[i].type.indexOf('sswarning') >= 0 || displayData[i].type.indexOf('ssup') >= 0);
+
+                if (orb1check || orb2check || orb3check /*|| orbWarningCheck || orbUpCheck || ssWarningCheck || ssUpCheck*/ || ssCheck) {
+                    if (lastText != displayData[i].text && seconds <= displayData[1].time) {
+                        lastText = displayData[i].text;
+
+                        if (voice) {
+                            if (orb1check || orb2check || orb3check) {
+                                singlebeep.play();
+                                responsiveVoice.speak(lastText.replace('{1}', orb1Val).replace('{2}', orb2Val).replace('{3}', orb3Val), "UK English Female");
+                            } else {
+                                singlebeep.play();
+                                responsiveVoice.speak(lastText, "US English Female");
+                            }
+                        } else if (chime) {
+                            if (orb1check || orb2check || orb3check) {
+                                upboing.play();
+                            } else {
+                                goingdownzip.play();
+                            }
+
                         } else {
-                            goingdownzip.play();
+                            //nothing
                         }
-
-                    } else {
-                        //nothing
                     }
+                    return displayData[i].text.replace('{1}', orb1Val).replace('{2}', orb2Val).replace('{3}', orb3Val);
+                } else {
+                    return "";
                 }
-                return displayData[i - 1].text;
-            } else {
-                return "";
             }
         }
     }
