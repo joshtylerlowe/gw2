6260e9a6752144898413535bc3fba3e0b9c1e48e
Josh Lowe <joshtylerlowe@gmail.com>
2018-05-27 23:32:57 -0600

    updated items json;

--- assets/javascript/ItemsLoader.js
+++ assets/javascript/ItemsLoader.js
@@ -1,6 +1,6 @@
 ﻿var allItems;
 
-$.getJSON('/assets/data/allItemIdsAndNames_4.22.18.json', function (response) {
+$.getJSON('/assets/data/allItemIdsAndNames_4.27.18.json', function (response) {
     allItems = response;
     $('.full-page-loading-spinner-container').hide();
 });
