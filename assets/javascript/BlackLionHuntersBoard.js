5d9e0dc27e6fac6b4c7b397a7a0b61fa2df0cc3d
Josh Lowe <josh.lowe@cvent.com>
2018-06-29 11:20:50 -0600

    updated items json; updated BlakLionHuntersBoard multipliers;

--- assets/javascript/BlackLionHuntersBoard.js
+++ assets/javascript/BlackLionHuntersBoard.js
@@ -1,10 +1,10 @@
 var multipliers = [
-    {tier:1, multiplier: 1.32},
+    {tier:1, multiplier: 1.37},
     {tier:2, multiplier: 0.68},
-    {tier:3, multiplier: 0.91},
-    {tier:4, multiplier: 0.74},
-    {tier:5, multiplier: 1.48},
-    {tier:6, multiplier: 0.43}
+    {tier:3, multiplier: 0.89},
+    {tier:4, multiplier: 0.72},
+    {tier:5, multiplier: 1.49},
+    {tier:6, multiplier: 0.44}
 ];
 var trophy = {
     bone: [
