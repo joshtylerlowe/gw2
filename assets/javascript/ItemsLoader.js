304b3baf1de45d4e32295495427636c4a3e39c28
Josh Lowe <josh.lowe@cvent.com>
2018-07-30 11:51:04 -0600

    updated items json;

--- assets/javascript/ItemsLoader.js
+++ assets/javascript/ItemsLoader.js
@@ -1,6 +1,6 @@
 ﻿var allItems;
 
-$.getJSON('/assets/data/allItemIdsAndNames_7.27.18.json', function (response) {
+$.getJSON('/assets/data/allItemIdsAndNames_7.30.18.json', function (response) {
     allItems = response;
     $('.full-page-loading-spinner-container').hide();
 });
--- updateItems.js
+++ updateItems.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 });
 
 var getAllKnownIdsAndNames = function() {
-    $.getJSON('/assets/data/allItemIdsAndNames_7.27.18.json', function(data) {
+    $.getJSON('/assets/data/allItemIdsAndNames_7.30.18.json', function(data) {
         allKnownItemIdsAndNames = data;
         updateItemIds();
     });
