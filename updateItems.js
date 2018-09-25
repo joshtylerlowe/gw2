af1bb904968eb186dda4974994e81441d03c6459
Josh Lowe <joshtylerlowe@gmail.com>
2018-09-25 14:10:38 -0600

    updated items json;

--- updateItems.js
+++ updateItems.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 });
 
 var getAllKnownIdsAndNames = function() {
-    $.getJSON('/assets/data/allItemIdsAndNames_9.13.18.json', function(data) {
+    $.getJSON('/assets/data/allItemIdsAndNames_9.24.18.json', function(data) {
         allKnownItemIdsAndNames = data;
         updateItemIds();
     });
